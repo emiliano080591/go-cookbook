80fafa2d6dbc9650b668f9feb69824068c1b9da6
emiliano080591 <emilianoruben91@gmail.com>
2021-10-08 12:57:29 -0500

    Actualizar main.go

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/emiliano080591/go-cookbook/database_mongo"
+import (
+	"fmt"
+	"github.com/emiliano080591/go-cookbook/async"
+	"github.com/emiliano080591/go-cookbook/database_mongo"
+	"net/http"
+)
 
 func main() {
 	//files
@@ -48,4 +53,20 @@ func main() {
 	//MONGO con interface intermedia
 	database_mongo.Exec2()
 
+	urls:=[]string{
+		"https://www.google.com",
+		"https://golang.org",
+		"https://www.github.com",
+	}
+	c:=async.NewClient(http.DefaultClient,len(urls))
+
+	async.FetchAll(urls,c)
+	for i:=0;i<len(urls);i++ {
+		select {
+		case resp:=<-c.Resp:
+			fmt.Printf("Status received for:%s:%d\n",resp.Request.URL,resp.StatusCode)
+		case err:=<-c.Err:
+			fmt.Printf("Error received:%s\n",err)
+		}
+	}
 }
