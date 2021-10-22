699284d0a47094b4cc18e030669bdd6acd7f2560
emiliano080591 <emilianoruben91@gmail.com>
2021-10-21 19:30:11 -0500

    FIX bugs

--- state/state.go
+++ state/state.go
@@ -1,1 +1,22 @@
 package state
+
+type Op string
+
+const (
+	Add       = "add"
+	Subtract  = "sub"
+	Multiply  = "mul"
+	Divide    = "div"
+)
+
+type WorkRequest struct {
+	Operation Op
+	Value1    int64
+	Value2    int64
+}
+
+type WorkResponse struct {
+	Wr     *WorkRequest
+	Result int64
+	Err    error
+}
